dd41b11c94de4672bc032ea1232e0d8309c203cf
BiswaJit0407 <biswajitpadhan2001@gmail.com>
2025-03-13 17:35:42 +0530

    weather

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Weather from './Weather'
+
+const App=()=> {
+ 
+  return (
+    <>
+      <div className='app'>
+        <Weather/>
+
+      </div>
+    </>
+  )
+}
+
+export default App
